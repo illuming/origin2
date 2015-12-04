3c6e89fccd9d6b05beb8077944c4b2f52d300b91
illuming <giulia.illuminati@roma1.infn.it>
2015-12-04 18:39:51 +0100

    Easier to understand

--- bounce.cc
+++ bounce.cc
@@ -7,6 +7,7 @@ int main() {
   double particleSpeed = 6.3;
 
   int maxColumn = 80;
+  int minColumn = 0;
   int timeStep = 0;
   int stopTime = 60;
 
@@ -19,8 +20,8 @@ int main() {
     if (particlePosition >= maxColumn) {
       particlePosition = maxColumn;
       particleSpeed = -particleSpeed;
-    } else if (particlePosition < 0) {
-      particlePosition = 0;
+    } else if (particlePosition < minColumn) {
+      particlePosition = minColumn;
       particleSpeed = -particleSpeed;
     }    
     timeStep++;
