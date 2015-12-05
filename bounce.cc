afe59f4de73ecc6987f884ef9570c334f5f6ed7c
illuming <giulia.illuminati@roma1.infn.it>
2015-12-05 13:42:42 +0100

    two particles

--- bounce.cc
+++ bounce.cc
@@ -6,12 +6,13 @@ const int minColumn = 0;
 char screen [80];
 
 void draw(double position, char symbol){
+
   for (int i = 0; i < position; i++) {
     screen[i] = ' ';
   }
   screen[(int) position]= symbol;
 
-  for (int i = 0; i < position; i++) {
+  for (int i = 0; i <= position; i++) {
     std::cout << screen[i] ;
   }
   std::cout<<"\n";
@@ -38,11 +39,17 @@ int main() {
   int timeStep = 0;
   int stopTime = 60;
 
+  char particleSymbol2 = 'y';
+  double particlePosition2 = 0;
+  double particleSpeed2 = 6.3;
 
   while (timeStep < stopTime) {
   
     draw (particlePosition, particleSymbol);
     particlePosition = move (particlePosition, particleSpeed);
+
+    draw (particlePosition2, particleSymbol2);
+    particlePosition2 = move (particlePosition2, particleSpeed2);
 
     timeStep++;
 
