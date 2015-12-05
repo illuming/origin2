97ce7f1725d3db3a6876b4de2385e126c14854ed
illuming <giulia.illuminati@roma1.infn.it>
2015-12-05 16:37:12 +0100

    added display and clear screen + passing parameter array screen both as a pointer and a array

--- bounce.cc
+++ bounce.cc
@@ -9,21 +9,12 @@ double Positions [n_particles]={0, 0, 0};
 double Speeds [n_particles]={6.3, 6.3, 6.3};
 char Symbols [n_particles] = {'x', 'y', 'z'};
 
-void draw(double position, char symbol){
-char screen [maxColumn+1];
-
+void draw(double position, char symbol, char screen[]){
   for (int i = 0; i < position; i++) {
     screen[i] = ' ';
   }
-  screen[(int) position]= symbol;
-
-  for (int i = 0; i <= position; i++) {
-    std::cout << screen[i];
-  }
-  std::cout<<"\n";
-  
+  screen[(int) position]= symbol; 
 }
-
 
 double move (double position, double &speed){ 
   position += speed;
@@ -37,19 +28,35 @@ double move (double position, double &speed){
   return position;
 }
 
+void display_screen (double position, char *screen){
+  for (int j = 0; j <= position; j++) {
+    std::cout << screen[j];
+  }
+  std::cout<<"\n";
+}
+
+void clear_screen (double position, char *screen) {
+  for (int j = 0; j <= position; j++) {
+    screen[j] = ' ';
+  }
+}
+
 int main() {
   int timeStep = 0;
   int stopTime = 60;
 
+  char screen [maxColumn+1];
+
   while (timeStep < stopTime) {
   
     for (int i=0; i<n_particles; i++){
-    draw (Positions[i], Symbols[i]);
-    Positions[i] = move (Positions[i], Speeds[i]);
+      draw (Positions[i], Symbols[i], screen);
+      display_screen (Positions[i], screen);
+      clear_screen (Positions[i], screen);
+      Positions[i] = move (Positions[i], Speeds[i]);
     }
 
     timeStep++;
-
   }
 
 }
