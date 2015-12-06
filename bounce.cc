63415849763c44b86846c170cda3178bd307d15c
illuming <giulia.illuminati@roma1.infn.it>
2015-12-06 19:01:31 +0100

    members name -> m_

--- bounce.cc
+++ bounce.cc
@@ -7,44 +7,44 @@ const int n_particles = 3;
 
 
 struct Particle {
-  char symbol;
-  double position;
-  double speed;
+  char m_symbol;
+  double m_position;
+  double m_speed;
   
-  void initialize (char sy, double po, double sp){
-    symbol = sy;
-    position = po;
-    speed = sp;  
+  void m_initialize (char sy, double po, double sp){
+    m_symbol = sy;
+    m_position = po;
+    m_speed = sp;  
   }
 
-  void draw (char screen[]){
-    for (int i = 0; i < position; i++) {
+  void m_draw (char screen[]){
+    for (int i = 0; i < m_position; i++) {
       screen[i] = ' ';
     }
-    screen[(int) position]= symbol; 
+    screen[(int) m_position]= m_symbol; 
   }
 
-  void move (){ 
-    position += speed;
-    if (position >= maxColumn) {
-      position = maxColumn;
-      speed = -speed;
-    } else if (position < minColumn) {
-      position = minColumn;
-      speed = -speed;
+  void m_move (){ 
+    m_position += m_speed;
+    if (m_position >= maxColumn) {
+      m_position = maxColumn;
+      m_speed = -m_speed;
+    } else if (m_position < minColumn) {
+      m_position = minColumn;
+      m_speed = -m_speed;
     }
   }
 };
 
 void display_screen (Particle const * const p, char *screen){
-  for (int j = 0; j <= p->position; j++) {
+  for (int j = 0; j <= p->m_position; j++) {
     std::cout << screen[j];
   }
   std::cout<<"\n";
 }
 
 void clear_screen (Particle const * const p, char *screen) {
-  for (int j = 0; j <= p->position; j++) {
+  for (int j = 0; j <= p->m_position; j++) {
     screen[j] = ' ';
   }
 }
@@ -57,17 +57,17 @@ int main() {
 
   Particle particles [n_particles];
 
-  particles[0].initialize ('x', 0, 6.3);
-  particles[1].initialize ('y', 0, 6.3);
-  particles[2].initialize ('z', 0, 6.3);
+  particles[0].m_initialize ('x', 0, 6.3);
+  particles[1].m_initialize ('y', 0, 6.3);
+  particles[2].m_initialize ('z', 0, 6.3);
 
   while (timeStep < stopTime) {
   
     for (int i=0; i<n_particles; i++){
-      particles[i].draw (screen);
+      particles[i].m_draw (screen);
       display_screen (&particles[i], screen);
       clear_screen (&particles[i], screen);
-      particles[i].move ();
+      particles[i].m_move ();
     }
 
     timeStep++;
